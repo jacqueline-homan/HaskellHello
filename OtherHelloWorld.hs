d4fe2bd14a90867f9fba2e6975ad1f8fcc457349
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-03-20 03:40:43 -0400

    trying out string length function

--- OtherHelloWorld.hs
+++ OtherHelloWorld.hs
@@ -4,3 +4,6 @@ import Happstack.Server(nullConf, Conf(..), simpleHTTP, toResponse, ok)
 
 main :: IO()
 main = simpleHTTP(nullConf {port=9000}) $ ok "Other hello world from port 9000!"
+
+sayHello :: String -> IO()
+sayHello x = putStrLn("Hello," ++ x ++ "!")
