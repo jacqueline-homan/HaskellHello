49bf9da329fb9240915868156789ee2811ce9c9f
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-03-20 03:32:49 -0400

    trying to make git commit here and getting frustrated

---
+++ OtherHelloWorld.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Happstack.Server(nullConf, Conf(..), simpleHTTP, toResponse, ok)
+
+main :: IO()
+main = simpleHTTP(nullConf {port=9000}) $ ok "Other hello world from port 9000!"
