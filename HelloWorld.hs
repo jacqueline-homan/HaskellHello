6d86f9f353bbabd69d7cf5fd9cd65136726bb7ce
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-10-16 09:21:03 -0400

    output message changed

--- HelloWorld.hs
+++ HelloWorld.hs
@@ -3,4 +3,4 @@ module Main where
 import Happstack.Server(nullConf, simpleHTTP, toResponse, ok)
 
 main :: IO()
-main = simpleHTTP nullConf $ ok "Hello, World! Yo, Evan I got Haskell on my Mac!"
+main = simpleHTTP nullConf $ ok "Hello, World! Yo, Evan I got Haskell on Debian!"
